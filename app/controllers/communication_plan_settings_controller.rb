68e1647770a763851bac36c22930b984a10823ab
Cassia Nino <cassianino@gmail.com>
2017-04-17 18:42:33 -0300

    Alterações para configurações gerais

--- app/controllers/communication_plan_settings_controller.rb
+++ app/controllers/communication_plan_settings_controller.rb
@@ -1,13 +1,12 @@
 class CommunicationPlanSettingsController < ApplicationController
   unloadable
 
-  def index
+  def show
     logger.info ">>> (^_^) [CommunicationPlanSettingsController#index]"
     #A tela de configuração geral deve apresentar os seguintes dados: estados do projeto, sinalizadores, logotipo e lista de macro atividades
     @project_statuses = WorkperformanceReportStatus.all
     @flags = Flag.all
     @logo = CommunicationPlanSettings.where(parameter: "logo")
-    puts ">> #{@logo}"
     @standard_activities = StandardMacroActivity.all
   end
 
